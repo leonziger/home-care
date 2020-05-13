f6ac0fdfa9071aaa184c177455e9dfb038e5eac5
Uma <buldog52@gmail.com>
2020-05-13 20:02:43 +0300

    Changed mobile menu

--- src/components/menu-toggle/menu-toggle.js
+++ src/components/menu-toggle/menu-toggle.js
@@ -2,8 +2,9 @@ import $ from 'jquery';
 
 $('.burger').click(function() {
   $('.menu-toggle').toggleClass('menu-toggle_active');
-  $('.main-header__nav').toggleClass('is-open');
-  $('body').toggleClass('overflow-hidden');
+  // $('.main-header__nav').toggleClass('is-open');
+  $('.main-menu').toggleClass('open');
+  $('body').toggleClass('menu-open');
 });
 
 $('#nav >ul li').each(function() {
@@ -12,12 +13,16 @@ $('#nav >ul li').each(function() {
   }
 });
 
-$('#nav li a').on('click', function(e){
-  if($(this).siblings('.drop').length && !$(this).closest('li').hasClass('drop-open')){
+$('#nav li a').on('click', function(e) {
+  if ( $(this).closest('li').hasClass('has-drop') ) {
     e.preventDefault();
-    $(this).closest('li').addClass('drop-open');
-    $(this).closest('li').siblings('li.drop-open').find('li.drop-open').removeClass('drop-open');
-    $(this).closest('li').siblings('li.drop-open').removeClass('drop-open');
+    const target = e.target.closest('li');
+
+    $(target).toggleClass('open');
+    if ($(target).hasClass('open')) {
+      $('.drop').slideDown();
+    } else {
+      $('.drop').slideUp();
+    }
   }
 });
-
